68d1e627147aa4f43b2b657a31f0ce0f7c8b7adc
Errytagedesign <errytage@gmail.com>
2021-09-17 08:43:19 +0100

    bug fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,7 @@ function Navbar() {
           breakpoint: 1024,
           settings: {
             slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToScroll: 6,
             infinite: true,
             
           }
@@ -34,7 +34,7 @@ function Navbar() {
           breakpoint: 600,
           settings: {
             slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToScroll: 6,
             initialSlide: 3
           }
         },
@@ -42,7 +42,7 @@ function Navbar() {
           breakpoint: 400,
           settings: {
             slidesToShow: 2,
-            slidesToScroll: 2,
+            slidesToScroll: 6,
             initialSlide: 2
           }
         }
