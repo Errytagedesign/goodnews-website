50d28cdeef080918211e2842b5f7a0d88defcc82
Errytagedesign <errytage@gmail.com>
2021-09-17 08:58:47 +0100

    bug fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,7 @@ function Navbar() {
         className: "center",
       infinite: true,
       centerPadding: "60px",
-      slidesToShow: 3,
+      slidesToShow: 5,
       swipeToSlide: true,
      
       responsive: [
@@ -25,7 +25,7 @@ function Navbar() {
           breakpoint: 1024,
           settings: {
             slidesToShow: 3,
-            slidesToScroll: 6,
+            slidesToScroll: 3,
             infinite: true,
             
           }
@@ -34,7 +34,7 @@ function Navbar() {
           breakpoint: 600,
           settings: {
             slidesToShow: 2,
-            slidesToScroll: 6,
+            slidesToScroll: 3,
             initialSlide: 2
           }
         },
@@ -42,7 +42,7 @@ function Navbar() {
           breakpoint: 400,
           settings: {
             slidesToShow: 2,
-            slidesToScroll: 6,
+            slidesToScroll: 3,
             initialSlide: 2
           }
         }
