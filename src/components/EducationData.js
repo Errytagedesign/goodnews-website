faaa67e49d7d6e9b9e70a613a363163605e3ef63
Errytagedesign <errytage@gmail.com>
2021-09-17 08:50:38 +0100

    bug fixed

--- src/components/EducationData.js
+++ src/components/EducationData.js
@@ -34,52 +34,74 @@ export  const EducationData = [
     {
       id: 2,
 
-      title: "",
+      title: "Septuagenarian Ph.D. Holder: The Incredible Story of Ifeyinwa Felicia Marinze",
 
       Name: "Good News",
 
-      Description: " ",
+      Description: " Not many people bag doctorate degrees at age 71, but Ifeyinwa Felicia Marinze did recently.    On Thursday, July 8, 2021, Marinze was formally awarded her Ph.D. in French/Linguistics in the Faculty of Arts at the 51st convocation of the University of Lagos (UNILAG)    With that feat, the native of Ukpor, Nnewi South Local Government, Anambra State, born on March 6, 1950, became the oldest Ph.D. graduate in the Nigerian university system.     ",
 
-      url: "",
+      url: "https://res.cloudinary.com/dmsyfdo0y/image/upload/v1631574366/News%20Article%20Images/Education_1_lcdoco.jpg",
   
       contents: [<div>
            
+           Not many people bag doctorate degrees at age 71, but Ifeyinwa Felicia Marinze did recently. 
+On Thursday, July 8, 2021, Marinze was formally awarded her Ph.D. in French/Linguistics in the Faculty of Arts at the 51st convocation of the University of Lagos (UNILAG)
+With that feat, the native of Ukpor, Nnewi South Local Government, Anambra State, born on March 6, 1950, became the oldest Ph.D. graduate in the Nigerian university system. <br /> <br />
+
+Her thesis was on “Evaluation de la Competence Communicative Orale du Francais Des Apprenants Nationale Diploma Des Polytechniques Seledionness du Nigeria.” <br /> <br />
+ 
+Commenting on her exploit at such an advanced age, Marinze said
+she never thought of her age when she enrolled in the doctoral programme. <br /> <br />
+ 
+"I told myself it was going to be a retirement project. There was this nagging problem in teaching my students all through my career as a French teacher. The students could not go beyond five sentences when engaged in oral communication in French. How can one not communicate well at the end of a two-year programme? <br /> <br />
+ 
+"What should a language teacher do, among other variables, to help them to surmount the challenge and achieve the ability to communicate with the Language? These were my focal points. I didn't want my brain to become static because once the brain is no longer tasked, the whole body will start giving way. I was not ready to be certified brain dead. I still want to contribute to knowledge and add value to society."
+The septuagenarian's first degree was at the University of Ibadan, French/English in Education, in 1980. She proceeded to acquire a master's in Curriculum Studies at UNILAG in 1985. She had another master's in French in UNILAG. She enrolled for M.Phil in 2011. She bagged a PhD from the Department of European Languages and International Studies, with matriculation number 849003042. <br /> <br />
+It took Marinze 41 years after her first degree and 36 years after her first master's degree to bag the PhD.
+ "God gave me patience, humility, determination and perseverance. I was a chief lecturer and director of UNEVOC Network, Yaba College of Technology, a platform for exchange, co-operation and mutual assistance."
+     Her feat is more remarkable as she saw herself through the programme. "I took it as my responsibility.  <br /> <br />All the trips I made to the states and field works used for the research were my personal cash. The expenses I incurred were basically from my purse. It was truly challenging, considering the security issues too. I was focused and God Almighty protected me all through the journeys and endeavours." <br /> <br />
+ Continuing about her experience, she disclosed that "The whole M.Phil/Ph.D took nine years. I am grateful to my first and second supervisors, Prof Osas Simire and Professor A. Ilupeju, for a job well done. Their support and advice gave me the strength I needed to push through. <br /> <br />
+The septuagenarian urged youths to dare to dream. "Place it before God and go after it. Don't look at your age. It is only a number and a distraction. Relax your mind, eat well and have some fun too. But most importantly, do not forget your Maker, God Almighty,"
+she said. <br /> <br />
 
 
       </div>]
     },
 
     {
-      id: 3,
-
-      title: "",
-
-      Name: "Good News",
-
-      Description: " ",
-
-      url: "",
-  
-      contents: [<div>
-           
-
-
-      </div>]
-    },
+        id: 3,
+        title: "Nigerian maths prodigy, Faith Odunsi, shines on the global stage",
+        Name: "Good News",
+        Description: "Fifteen-year-old Faith Odunsi recently made Nigeria proud as she emerged the Global Open Mathematics Tournament winner, an international competition with participants from Europe, Africa, America, Asia, and Australia. ",
+        url: "https://res.cloudinary.com/dmsyfdo0y/image/upload/v1631574368/News%20Article%20Images/Education_2_ns7tlh.png",
+    
+        contents: [`Fifteen-year-old Faith Odunsi recently made Nigeria proud as she emerged the Global Open Mathematics Tournament winner, an international competition with participants from Europe, Africa, America, Asia, and Australian`, <br />, <br />, `Odunsi, an SS3 student from Ijebu in Ogun state, shared her experience winning the global competition in an interview with The Punch.`, `She disclosed that winning the 'tough competition' made her happy and honored. Odunsi also narrated how she excelled in different stages of the contest to emerge the overall winner.`, <br />, <br />,  `Her words: In the first stage, there was a computer-based test, and we had to answer many questions in one hour. My score in the test was 66. The second stage took the same format, and I also scored 66.`, 'The quarter and semi-finals were on Microsoft Teams. We were asked questions, and we put in our answers; the first to put in the correct answers got 10 points.', <br />, <br />,  `Revealing how she prepared for the contest, Odunsi said she made sure the competition did not affect her academic activities. She had after-school lessons and also studied at night from 11 pm to 12 am. As the winner of the contest, Faith was presented with a cash prize of $1,000.`, <br />, <br />, 
+         `Before winning the global contest, Faith had in 2018 set a record in Cowbellpedia by answering 19 questions in 60 seconds. See the link below.`] 
+      },
 
     {
       id: 4,
 
-      title: "",
+      title: "UNILAG alumnus wins American Bar Association’s human rights award",
 
       Name: "Good News",
 
       Description: " ",
 
-      url: "",
+      url: "https://res.cloudinary.com/dmsyfdo0y/image/upload/v1631574367/News%20Article%20Images/Education_6_hgxkrt.jpg",
   
       contents: [<div>
            
+                Adeola Austin Oyinlade, an alumnus of the University of Lagos has become the first Nigerian to win the American Bar Association International Human Rights Award 2021.
+        In a post on its website, UNILAG commended Oyinlade for the award, noting that its alumnus is the first Nigerian to bag the award. <br /> <br />
+        “The Human Rights and International Law expert was honored in recognition of his works in advancing human rights solutions in Africa. <br /> <br />
+        “On behalf of the entire university community, the Vice-Chancellor, Professor Oluwatoyin Ogundipe, FAS, congratulates Mr. Adeola Austin Oyinlade on his achievement and wishes him continued success,” the post added. <br /> <br />
+        According to Jinny Choi, in a statement released on behalf of the Association, the award was presented to  Oyinlade “in recognition of his exceptional commitment and contribution to the advancement of human rights in Nigeria”. <br /> <br />
+        In her address, the ABA President, Patricia Lee Refo, described  Oyinlade as one who has “advanced solutions to human rights issues across Africa, including the South Sudan political crisis, the Central African Republic crisis, the Congo Democratic Republic armed conflict and Libyan peace talks”. <br /> <br />
+        This is the second time in three years that the Nigerian lawyer will be recognized by the global legal community for his dedication to Human Rights. In October 2018, Mr. Oyinlade was named the International Bar Association (IBA) in Rome, Italy, the world body of Lawyers, International Bar Association (IBA) Human Rights Lawyer of the Year for his outstanding contribution as a Legal Practitioner to Human Rights advancement. <br /> <br />
+        Oyinlade is the Principal Partner of Adeola Oyinlade & Co, a law firm based in Lagos.
+        He is also the Founder of Constitutional Rights Awareness and Liberty Initiative, an NGO dedicated to expanding the frontiers of liberty, human rights, democracy, and happiness of people in Nigeria.
+        He graduated from the Faculty of Law with a Bachelor of Laws (LLB) in 2011 and bagged his Master of Laws (LLM) degree in 2014 from the same faculty. <br />
 
 
       </div>]
